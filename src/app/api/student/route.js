3a9f6dbafe1661ed2b348f60651cb53d3a2712a5
JWJY <warisara.jy@gmail.com>
2023-09-04 16:03:40 +0700

    lab16

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -1,5 +1,6 @@
 import { DB } from "@/app/libs/DB";
 import {
+  zStudentDeleteBody,
   zStudentGetParam,
   zStudentPostBody,
   zStudentPutBody,
@@ -29,8 +30,12 @@ export const GET = async (request) => {
   if (program !== null) {
     filtered = filtered.filter((std) => std.program === program);
   }
-
+  
   //filter by student id here
+
+  if (studentId !== null) {
+    filtered = filtered.filter((std) => std.studentId === studentId);
+  }
 
   return NextResponse.json({ ok: true, students: filtered });
 };
@@ -98,6 +103,7 @@ export const PUT = async (request) => {
 
 export const DELETE = async (request) => {
   //get body and validate it
+  const body = await request.json();
 
   //check if student id exist
 
@@ -108,8 +114,33 @@ export const DELETE = async (request) => {
   //or 2. use splice array method
   // DB.students.splice(...)
 
+  const parseResult = zStudentDeleteBody.safeParse(body);
+  if (parseResult.success === false) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: parseResult.error.issues[0].message,
+      },
+      { status: 400 }
+    );
+  }
+
+  const foundIndex = DB.students.findIndex(
+    (std) => std.studentId === body.studentId
+  );
+  if (foundIndex === -1) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Student ID does not exist",
+      },
+      { status: 404 }
+    );
+  }
+  
+  DB.students = DB.students.filter((std) => (std.studentId !== body.studentId));
   return NextResponse.json({
     ok: true,
-    message: `Student Id xxx has been deleted`,
+    message: `Student Id ${body.studentId} has been deleted`,
   });
 };
